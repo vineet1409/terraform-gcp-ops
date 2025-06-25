36328aa740cde0124422c65cf9a2bffca29c8f69
Vineet Srivastava <srivineet93@gmail.com>
2025-06-25 00:16:38 -0500

    Update main.tf

--- modules/vertex_endpoint/main.tf
+++ modules/vertex_endpoint/main.tf
@@ -1,5 +1,9 @@
-
 resource "google_vertex_ai_endpoint" "endpoint" {
+  name         = "${var.endpoint_name}-${random_id.suffix.hex}"
   display_name = var.endpoint_name
   location     = var.region
+}
+
+resource "random_id" "suffix" {
+  byte_length = 2
 }
