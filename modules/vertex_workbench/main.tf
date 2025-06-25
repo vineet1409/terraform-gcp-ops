7270075af182c71427378a33e45cda21a0e3b960
Vineet Srivastava <srivineet93@gmail.com>
2025-06-25 00:18:21 -0500

    Update main.tf

--- modules/vertex_workbench/main.tf
+++ modules/vertex_workbench/main.tf
@@ -1,11 +1,19 @@
+resource "google_workbench_instance" "workbench" {
+  name     = var.instance_name
+  location = var.location
 
-resource "google_notebooks_instance" "workbench" {
-  name         = var.instance_name
-  location     = var.location
-  machine_type = var.machine_type
+  gce_setup {
+    machine_type = var.machine_type
+    disable_public_ip = false
 
-  vm_image {
-    project      = "deeplearning-platform-release"
-    image_family = "tf2-ent-2-8-cpu"
+    boot_disk {
+      disk_type = "pd-standard"
+      disk_size_gb = 100
+    }
+
+    metadata = {
+      "proxy-mode" = "service_account"
+    }
   }
+  # Set the VM image as you wish, or leave for default
 }
